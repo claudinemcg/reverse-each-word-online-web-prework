9384ea1fc1985f3dcb312e893b8eeabf797aba7b
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-08 06:23:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,12 @@
-def reverse_each_word(word)
-  word.split()
-  
+
+
+
+
+def reverse_each_word(string)
+  array = string.split()
+  new_arr = []
+  array.collect do |word|
+    new_arr << word.reverse
+  end
+  return new_arr.join(" ")
 end
